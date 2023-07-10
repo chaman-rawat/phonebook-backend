228e29f842ded0dd96a06271ddb4b9d3ecf17e02
Chaman Rawat <chamanrawat.dev@gmail.com>
2023-07-11 01:23:44 +0530

    uibuild

---
+++ mongo.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const url = `mongodb+srv://chaman619:${password}@cluster0.6bda6do.mongodb.net/phonebookdb?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (process.argv.length === 3) {
+  console.log("phonebook:");
+  Person.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(person);
+    });
+    mongoose.connection.close();
+  });
+} else if (process.argv.length === 5) {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  });
+  //   console.log(person);
+  person.save().then((result) => {
+    console.log(`added ${person.name} number ${person.number} to phonebook`);
+    mongoose.connection.close();
+  });
+}
